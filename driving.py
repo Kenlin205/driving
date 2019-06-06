c595da66f97df90ae7e59fb77d86a20cb3c6d90b
Ken Lin <kenlin205@gmail.com>
2019-06-07 05:46:48 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input("where are you come from? ")
+age = input("what is your age? ")
+age = int(age)
+if country == "Taiwan":
+	if age >= 18:
+		print("You have driving licence qualify")
+	else:
+		print("You do't have driving licence qualify  ")
